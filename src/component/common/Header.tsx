72dbbcaf4a6d70964817b09542117be3ef54ae50
Kim-Haul <bokugawa@naver.com>
2023-02-27 12:13:34 +0900

    :lipstick: 헤더 fixed 및 hover 모달 박스 생성 & QR 코드 추가

--- src/component/common/Header.tsx
+++ src/component/common/Header.tsx
@@ -11,8 +11,39 @@ const Header = () => {
         </div>
         <div className="header-right">
           <span>로그아웃</span>
-          <MdDashboardCustomize />
-          <div className="modal">안녕하세요</div>
+          <div className="modal">
+            <MdDashboardCustomize />
+            <div className="modal-desc">
+              <div className="modal-desc-title">제작자 컨택하기</div>
+              <div className="modal-desc-content">
+                <div className="content-left">
+                  <img src="/images/qr_code.svg" alt="큐알코드" />
+                </div>
+                <div className="content-right">
+                  <a
+                    href="https://www.instagram.com/in.__ho/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div>
+                      <img src="/images/instagram.png" alt="큐알코드" />
+                      인스타그램
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/Kim-Haul/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div>
+                      <img src="/images/github.png" alt="큐알코드" />
+                      깃허브
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Frame>
     </Wrap>
@@ -44,24 +75,81 @@ const Frame = styled.div`
     display: flex;
     align-items: center;
     height: 100%;
-    gap: 30px;
+    gap: 10px;
     span {
       cursor: pointer;
     }
-    svg {
-      font-size: 2rem;
-      color: #e1e1e1;
-      cursor: pointer;
-    }
     .modal {
-      position: absolute;
-      background-color: #fff;
-      border: 1px solid #e1e1e1;
-      top: 60px;
-      right: 0;
-      width: 255px;
-      height: 200px;
-      padding: 1rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 50px;
+      cursor: pointer;
+      svg {
+        font-size: 2rem;
+        color: #e1e1e1;
+      }
+      .modal-desc {
+        display: none;
+        cursor: default;
+        &:hover {
+          display: block;
+        }
+        position: absolute;
+        background-color: #fff;
+        border: 1px solid #e1e1e1;
+        top: 60px;
+        right: 0;
+        width: 255px;
+        height: 133px;
+        color: #000;
+        .modal-desc-title {
+          background-color: #f8f8f8;
+          border-bottom: 1px solid #e1e1e1;
+          color: gray;
+          padding: 7px;
+        }
+        .modal-desc-content {
+          padding: 10px;
+          display: flex;
+          gap: 10px;
+          .content-left {
+            img {
+              width: 80px;
+              height: 80px;
+            }
+          }
+          .content-right {
+            display: flex;
+            flex-direction: column;
+            margin-top: 2px;
+            gap: 12px;
+            div {
+              cursor: pointer;
+              border: 1px solid #e1e1e1;
+              color: gray;
+              width: 120px;
+              padding: 7px;
+              display: flex;
+              gap: 5px;
+              img {
+                width: 15px;
+                height: 15px;
+              }
+              &:hover {
+                background-color: rgba(0, 123, 255, 0.1);
+                color: #35a3dc;
+              }
+            }
+          }
+        }
+      }
+      &:hover {
+        .modal-desc {
+          display: block;
+        }
+      }
     }
   }
 `;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// 컴포넌트 임포트
+import Chart from '../component/core/Chart';
+import List from '../component/core/List';
+import Trade from '../component/core/Trade';
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <Wrap>
+      <div></div>
+    </Wrap>
+  );
 };
 
 export default Dashboard;
+const Wrap = styled.div``;
